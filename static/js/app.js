88fad5e5f0250a96d197506893e05b835df274bd
HavyJavy <portocarrero.javier@gmail.com>
2020-11-12 13:43:12 -0600

    populate table

--- static/js/app.js
+++ static/js/app.js
@@ -9,15 +9,23 @@ console.log(data)
 // populate table
 
 data.forEach(data => {
-var table = tbody.append("tr");
-Object.entries(data).forEach(([key, value])=>{
-var tableRow = table.append("td");
-tableRow.text(value);
+    var table = tbody.append("tr");
+    Object.entries(data).forEach(([key, value]) => {
+        var tableRow = table.append("td");
+        tableRow.text(value);
 
-});
+    });
 
 });
 
 let button = d3.select("#filter-btn");
 
+button.on("click", function () {
 
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value")
+    console.log(inputValue);
+
+    var filteredData = tableData.filter(date === inputValue);
+    console.log(filteredData);
+})
